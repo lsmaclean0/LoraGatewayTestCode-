0f8b8b20960f43b26e2b9a40f09a9d558850a8e5
lastcow <zhijiang@chen.me>
2018-07-22 22:46:40 -0400

    init

--- retrocow_lora_gateway.cpp
+++ retrocow_lora_gateway.cpp
@@ -91,6 +91,140 @@ double optFQ = -1.0;
 uint8_t optSW = 0x12;
 
 
+uint8_t loraMode = LORAMODE;
+
+
+/**
+ * Lora Module Configuraiton
+ */
+void startConfig(){
+
+    int e;
+
+    // Set transmission mode and print the result
+    PRINT_CSTSTR("%s", "^$LoRa mode ");
+    PRINT_VALUE("%d", loraMode);
+    PRINTLN;
+
+    e = sx1272.setMode(loraMode);
+    PRINT_CSTSTR("%s", "^$Setting mode: state ");
+    PRINT_VALUE("%d", e);
+    PRINTLN;
+
+    // Select frequency channel
+    if (loraMode == 11) {
+        // if we start with mode 11, then switch to 868.1MHz for LoRaWAN test
+        // Note: if you change to mode 11 later using command /@M11# for instance, you have to use /@C18# to change to the correct channel
+        e = sx1272.setChannel(CH_18_868);
+        PRINT_CSTSTR("%s", "^$Channel CH_18_868: state ");
+    } else {
+        e = sx1272.setChannel(loraChannel);
+
+        if (optFQ > 0.0) {
+            PRINT_CSTSTR("%s", "^$Frequency ");
+            PRINT_VALUE("%f", optFQ);
+            PRINT_CSTSTR("%s", ": state ");
+        } else {
+            #ifdef BAND868
+                if (loraChannelIndex>5) {
+                  PRINT_CSTSTR("%s","^$Channel CH_1");
+                  PRINT_VALUE("%d", loraChannelIndex-6);
+                }
+                else {
+                  PRINT_CSTSTR("%s","^$Channel CH_0");
+                  PRINT_VALUE("%d", loraChannelIndex+STARTING_CHANNEL);
+                }
+                PRINT_CSTSTR("%s","_868: state ");
+            #elif defined BAND900
+                PRINT_CSTSTR("%s", "^$Channel CH_");
+                PRINT_VALUE("%d", loraChannelIndex);
+                PRINT_CSTSTR("%s", "_900: state ");
+            #elif defined BAND433
+                //e = sx1272.setChannel(0x6C4000);
+                PRINT_CSTSTR("%s","^$Channel CH_");
+                PRINT_VALUE("%d", loraChannelIndex);
+                PRINT_CSTSTR("%s","_433: state ");
+            #endif
+        }
+    }
+
+    PRINT_VALUE("%d", e);
+    PRINTLN;
+
+    // Select amplifier line; PABOOST or RFO
+    #ifdef PABOOST
+        sx1272._needPABOOST=true;
+        // previous way for setting output power
+        // loraPower='x';
+        PRINT_CSTSTR("%s","^$Use PA_BOOST amplifier line");
+        PRINTLN;
+    #else
+        // previous way for setting output power
+        // loraPower='M';
+    #endif
+
+    // Select output power in dBm
+    e = sx1272.setPowerDBM((uint8_t) MAX_DBM);
+
+    PRINT_CSTSTR("%s", "^$Set LoRa power dBm to ");
+    PRINT_VALUE("%d", (uint8_t) MAX_DBM);
+    PRINTLN;
+
+    PRINT_CSTSTR("%s", "^$Power: state ");
+    PRINT_VALUE("%d", e);
+    PRINTLN;
+
+    // get preamble length
+    e = sx1272.getPreambleLength();
+    PRINT_CSTSTR("%s", "^$Get Preamble Length: state ");
+    PRINT_VALUE("%d", e);
+    PRINTLN;
+    PRINT_CSTSTR("%s", "^$Preamble Length: ");
+    PRINT_VALUE("%d", sx1272._preamblelength);
+    PRINTLN;
+
+    if (sx1272._preamblelength != 8) {
+        PRINT_CSTSTR("%s", "^$Bad Preamble Length: set back to 8");
+        sx1272.setPreambleLength(8);
+        e = sx1272.getPreambleLength();
+        PRINT_CSTSTR("%s", "^$Get Preamble Length: state ");
+        PRINT_VALUE("%d", e);
+        PRINTLN;
+        PRINT_CSTSTR("%s", "^$Preamble Length: ");
+        PRINT_VALUE("%d", sx1272._preamblelength);
+        PRINTLN;
+    }
+
+    // Set the node address and print the result
+    //e = sx1272.setNodeAddress(loraAddr);
+    sx1272._nodeAddress = loraAddr;
+    e = 0;
+    PRINT_CSTSTR("%s", "^$LoRa addr ");
+    PRINT_VALUE("%d", loraAddr);
+    PRINT_CSTSTR("%s", ": state ");
+    PRINT_VALUE("%d", e);
+    PRINTLN;
+
+    if (optAESgw)
+        PRINT_CSTSTR("%s", "^$Handle AES encrypted data\n");
+
+    if (optRAW) {
+        PRINT_CSTSTR("%s", "^$Raw format, not assuming any header in reception\n");
+        // when operating n raw format, the SX1272 library do not decode the packet header but will pass all the payload to stdout
+        // note that in this case, the gateway may process packet that are not addressed explicitly to it as the dst field is not checked at all
+        // this would be similar to a promiscuous sniffer, but most of real LoRa gateway works this way
+        sx1272._rawFormat = true;
+    }
+
+    // Print a success message
+    PRINT_CSTSTR("%s", "^$SX1272/76 configured ");
+    PRINT_CSTSTR("%s", "as LR-BS. Waiting RF input for transparent RF-serial bridge\n");
+    #if defined ARDUINO && defined GW_RELAY
+        PRINT_CSTSTR("%s","^$Act as a simple relay gateway\n");
+    #endif
+
+}
+
 /**
  * Setup Lora Device
  */
@@ -125,6 +259,8 @@ void setup(){
 
     if (!e){
         radioON = true;
+        // Start configuration
+        startConfig();
     }
 
     FLUSHOUTPUT;
