dcf0351e5ba13f88dfcd310dc9089350d8501425
lastcow <zhijiang@chen.me>
2018-07-27 02:01:24 -0400

    display position adjusted.

--- scripts/retrocow_lora_gw.py
+++ scripts/retrocow_lora_gw.py
@@ -128,10 +128,10 @@ def displayData(temp):
         draw.text((0, 0), "RetroCow Radio GW", fill="white")
         if device.height >= 32:
             draw.text((0, 14), "Temp: " + temp, fill="white")
-            draw.text((0, 45), '____________________', fill="white")
+            draw.text((0, 50), '____________________', fill="white")
             #draw time at bottom
             now = datetime.now()
-            draw.text((0, 56), "U: " + now.strftime('%x')+ ' ' + now.strftime('%X'), fill="white")
+            draw.text((0, 60), "U: " + now.strftime('%x')+ ' ' + now.strftime('%X'), fill="white")
 
 def publishToGoogleSub(message):
     # UTF-8 encoding
